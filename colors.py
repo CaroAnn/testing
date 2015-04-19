2c0e02cd5620ad7628f244d1b88cc8aad6f525af
CaroAnn <cas489@cornell.edu>
2015-04-19 13:56:23 -0400

    Update colors.py

--- colors.py
+++ colors.py
@@ -1,2 +1,10 @@
 #test
+#once you save something as _____.py you can get the colors, but you have to save it first
 
+input rospy
+
+for i in range(10):
+    apple = i+2
+    print apple
+
+print apple
